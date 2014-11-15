409ef284e43bac42bb18004cdcf5ed2a8dfc47c7
admin <admin@zyj>
2014-11-15 14:42:43 +0800

    splash init

--- MobileSafe/src/com/zengyan/mobilesafe/SplashActivity.java
+++ MobileSafe/src/com/zengyan/mobilesafe/SplashActivity.java
@@ -2,20 +2,41 @@ package com.zengyan.mobilesafe;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class SplashActivity extends ActionBarActivity {
 
+	private TextView tvVersion;
+	private PackageManager packageManager;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash_activity);
+		tvVersion=(TextView) findViewById(R.id.tv_version);
+		tvVersion.setText("°æ±¾£º"+getVersion());
+	}
+	private String getVersion() {
+		packageManager=getPackageManager();
+		try {
+		PackageInfo info=	packageManager.getPackageInfo(getPackageName(), 0);
+		return info.versionName;
+			
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "";
+		}
 	}
 
+	
 
 }
---
+++ MobileSafe/src/com/zengyan/mobilesafe/utils/Tools.java
@@ -0,0 +1,17 @@
+package com.zengyan.mobilesafe.utils;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.R.string;
+
+public class Tools {
+
+	public String dataService(string url) {
+		
+		HttpClient client=new DefaultHttpClient();
+		
+		
+		return null;
+	}
+}
