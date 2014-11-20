15d966657bf2a438dded6bda8c0dee2cf9bf3f72
zengjunqi <zengjunqi@github.com>
2014-11-20 17:50:55 +0800

    增加来电提醒

--- MobileSafe/src/com/zengyan/mobilesafe/SettingActivity.java
+++ MobileSafe/src/com/zengyan/mobilesafe/SettingActivity.java
@@ -1,52 +1,155 @@
 package com.zengyan.mobilesafe;
 
-import com.zengyan.mobilesafe.ui.SettingItem;
-
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 
+import com.zengyan.mobilesafe.service.AddressService;
+import com.zengyan.mobilesafe.ui.SettingClickView;
+import com.zengyan.mobilesafe.ui.SettingItem;
+import com.zengyan.mobilesafe.utils.ServiceUtils;
 
 public class SettingActivity extends Activity {
 	private SettingItem siv_update;
 	private SharedPreferences sp;
+
+	// 设置是否开启显示归属地
+	private SettingItem siv_show_address;
+	private Intent showAddress;
+
+	// 设置归属地显示框背景
+	private SettingClickView scv_changebg;
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		showAddress = new Intent(this, AddressService.class);
+		boolean isServiceRunning = ServiceUtils.isServiceRunning(
+				SettingActivity.this,
+				"com.itheima.mobilesafe.service.AddressService");
+
+		if (isServiceRunning) {
+			// 监听来电的服务是开启的
+			siv_show_address.setChecked(true);
+		} else {
+			siv_show_address.setChecked(false);
+		}
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_setting);
 		sp = getSharedPreferences("config", MODE_PRIVATE);
 		siv_update = (SettingItem) findViewById(R.id.siv_update);
-		
+
 		boolean update = sp.getBoolean("update", false);
-		if(update){
-			//自动升级已经开启
+		if (update) {
+			// 自动升级已经开启
 			siv_update.setChecked(true);
-		}else{
-			//自动升级已经关闭
+		} else {
+			// 自动升级已经关闭
 			siv_update.setChecked(false);
 		}
 		siv_update.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				Editor editor = sp.edit();
-				//判断是否有选中
-				//已经打开自动升级了
-				if(siv_update.isChecked()){
+				// 判断是否有选中
+				// 已经打开自动升级了
+				if (siv_update.isChecked()) {
 					siv_update.setChecked(false);
 					editor.putBoolean("update", false);
-					
-				}else{
-					//没有打开自动升级
+
+				} else {
+					// 没有打开自动升级
 					siv_update.setChecked(true);
 					editor.putBoolean("update", true);
 				}
 				editor.commit();
 			}
 		});
+
+		// 设置号码归属地显示空间
+		siv_show_address = (SettingItem) findViewById(R.id.siv_show_address);
+		showAddress = new Intent(this, AddressService.class);
+		boolean isServiceRunning = ServiceUtils.isServiceRunning(
+				SettingActivity.this,
+				"com.zengyan.mobilesafe.service.AddressService");
+
+		if (isServiceRunning) {
+			// 监听来电的服务是开启的
+			siv_show_address.setChecked(true);
+		} else {
+			siv_show_address.setChecked(false);
+		}
+
+		siv_show_address.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				if (siv_show_address.isChecked()) {
+					// 变为非选中状态
+					siv_show_address.setChecked(false);
+					stopService(showAddress);
+
+				} else {
+					// 选择状态
+					siv_show_address.setChecked(true);
+					startService(showAddress);
+
+				}
+
+			}
+		});
+
+		// 设置号码归属地的背景
+		scv_changebg = (SettingClickView) findViewById(R.id.scv_changebg);
+		scv_changebg.setTitle("归属地提示框风格");
+		final String[] items = { "半透明", "活力橙", "卫士蓝", "金属灰", "苹果绿" };
+		int which = sp.getInt("which", 0);
+		scv_changebg.setDesc(items[which]);
+
+		scv_changebg.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				int dd = sp.getInt("which", 0);
+				// 弹出一个对话框
+				AlertDialog.Builder builder = new Builder(SettingActivity.this);
+				builder.setTitle("归属地提示框风格");
+				builder.setSingleChoiceItems(items, dd,
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+
+								// 保存选择参数
+								Editor editor = sp.edit();
+								editor.putInt("which", which);
+								editor.commit();
+								scv_changebg.setDesc(items[which]);
+
+								// 取消对话框
+								dialog.dismiss();
+							}
+						});
+				builder.setNegativeButton("cancel", null);
+				builder.show();
+
+			}
+		});
+
 	}
 
 }
